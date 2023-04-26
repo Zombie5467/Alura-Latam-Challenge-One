4e35b0fef7651e02473a7e3c591a9cbb23cd8a76
Addel <sigmamediacode@gmail.com>
2023-04-26 18:27:13 -0400

    cambio de carpeta

--- futbol.js
+++
@@ -1,6 +0,0 @@
-var victorias = parseInt(prompt("numero de victorias"));
-var empates = parseInt(prompt("cantidad de empates"));
-
-var total = (victorias * 3) + empates;
-
-console.log(`El total de puntos de tu equipo es ${total}`);
